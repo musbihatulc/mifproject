25ed7fcfec498989774216e975444250f00d0c4b
musbihatulc <57608234+musbihatulc@users.noreply.github.com>
2020-12-15 13:30:51 +0700

    ganti
    ganti grafik

--- application/views/grafik_ijazah.php
+++ application/views/grafik_ijazah.php
@@ -9,7 +9,7 @@
 <br>
 
 <a href="<?php echo base_url(); ?>Home" class="btn btn-primary">Kembali</a>
-<a href="<?php echo base_url(); ?>C_grafik/SimpanIjazah" class="btn btn-success">Simpan</a>
+<a href="<?php echo base_url(); ?>C_grafik/SimpanIjazah" class="btn btn-success">Simpan</a> 
 <h1 style="text-align:center">Grafik Tingkat Ijazah</h1>
 <canvas id="myChart"></canvas>
     <?php
@@ -28,13 +28,13 @@
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
-        type: 'bar',
+        type: 'pie',
         // The data for our dataset
         data: {
             labels: [<?php echo $tingkat_ijazah; ?>],
             datasets: [{
                 label:'Grafik',
-                backgroundColor: ['rgb(255, 99, 132)', 'rgba(56, 86, 255, 0.87)', 'rgb(60, 179, 113)','rgb(175, 238, 239)', 'rgb(128, 0, 0)', 'rgb(128, 128, 0)', 'rgb(255, 255, 0)', 'RGB(233, 150, 122)', 'RGB(0, 0, 128)'],
+                backgroundColor: ['rgb(138, 43, 226)' ,'rgb(210, 105, 30)','rgb(255, 99, 132)', 'rgba(56, 86, 255, 0.87)', 'rgb(60, 179, 113)','rgb(175, 238, 239)', 'rgb(128, 0, 0)', 'rgb(128, 128, 0)', 'rgb(255, 255, 0)', 'RGB(233, 150, 122)', 'RGB(0, 0, 128)'],
                 borderColor: ['rgb(255, 99, 132)'],
                 data: [<?php echo $jumlah; ?>]
             }]
@@ -42,7 +42,7 @@
         // Configuration options go here
         options: {
             scales: {
-                yAxes: [{
+                Axes: [{
                     ticks: {
                         beginAtZero:true
                     }
@@ -51,6 +51,14 @@
         }
     });
 </script>
+
+<!-- <script>
+		 window.print();
+</script>
+
+ <style type="text/css" media="print">
+ @page { size: auto; }
+</style> -->
 
 </body>
 </html>
